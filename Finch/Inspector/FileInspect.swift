3a0a8c835354fb89adc33422ecf2b9bf949d331b
Dorian <6209874+MrSkwiggs@users.noreply.github.com>
2023-02-17 15:20:32 +0100

    🔥 Remove commented-out code

--- Finch/Inspector/FileInspect.swift
+++ Finch/Inspector/FileInspect.swift
@@ -13,57 +13,16 @@ struct FileInspect: View {
     var viewModel: ViewModel
     
     var body: some View {
-//        List {
-            VStack(alignment: .center, spacing: 20) {
-                RoundedRectangle(cornerRadius: 36, style: .continuous)
-                    .fill(Color.gray.opacity(0.3))
-                    .frame(width: 150, height: 150)
-                (Text("\(viewModel.appInfo.name) (v ")
-                 + Text(viewModel.appInfo.marketingVersion)
-                    .bold()
-                 + Text(")"))
-                .font(.title)
-                
-            }
-//            .frame(maxWidth: .infinity)
-//            Section("Files") {
-//                ScrollView {
-//                    VStack(alignment: .leading) {
-//                        ForEach(viewModel.appInfo.filePaths, id: \.self) { path in
-//                            Text(path)
-//                                .foregroundColor(.gray)
-//                        }
-//                    }
-//                    .padding()
-//                }
-//                .frame(maxWidth: .infinity, minHeight: 100, maxHeight: 100)
-//                .background(
-//                    RoundedRectangle(cornerRadius: 8)
-//                        .fill(Color.gray.opacity(0.3))
-//                )
-//            }
-//        }
-    }
-    
-    private var structure: [AppStructure] {
-        [
-            .init(name: "Files",
-                  sections: viewModel.appInfo.filePaths.map({
-                      .init(name: $0)
-                  }))
-        ]
-    }
-}
-
-extension FileInspect {
-    struct AppStructure: Identifiable {
-        let id = UUID()
-        let name: String
-        let sections: [AppStructure]?
-        
-        init(name: String, sections: [AppStructure]? = nil) {
-            self.name = name
-            self.sections = sections
+        VStack(alignment: .center, spacing: 20) {
+            RoundedRectangle(cornerRadius: 36, style: .continuous)
+                .fill(Color.gray.opacity(0.3))
+                .frame(width: 150, height: 150)
+            (Text("\(viewModel.appInfo.name) (v ")
+             + Text(viewModel.appInfo.marketingVersion)
+                .bold()
+             + Text(")"))
+            .font(.title)
+            Text("\(viewModel.appInfo.filePaths.count) file(s)")
         }
     }
 }
--- Finch/RootView/RootView.swift
+++ Finch/RootView/RootView.swift
@@ -34,7 +34,6 @@ struct RootView: View {
                 }))
             }
         }
-        
     }
 }
 
