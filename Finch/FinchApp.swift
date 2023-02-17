9b0526de9294cdcdcde45f6a15e46b3c1c6dfdea
Dorian <6209874+MrSkwiggs@users.noreply.github.com>
2023-02-17 10:59:43 +0100

    🎉 Create App

---
+++ Finch/FinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinchApp.swift
+//  Finch
+//
+//  Created by Dorian on 17/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct FinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
